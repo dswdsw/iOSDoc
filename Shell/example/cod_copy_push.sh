37be485987d2f8260b606d42823e16421fdb0ffe
stephen <dingsw@tuya.com>
2018-10-27 16:44:25 +0800

    Update Shell/example/cod_copy_push.sh

--- Shell/example/cod_copy_push.sh
+++ Shell/example/cod_copy_push.sh
@@ -1,36 +1,80 @@
 #!/bin/bash
 
-function getModuledir(){
-    for element in `ls $1`
-    do  
-        copy $1 $element
-    done
+# 获取分支名
+function getBranchName()
+{
+  cd  $1
+
+  result=`git branch | grep '^*' `
+
+  result=(${result//\*})
+
+  echo $result
 }
 
-function copy(){
+# 获取pod中模块分支名
+function getPodModuleBranchName(){
+   
+  doc=$1
 
-    new_dir=$1"/TuyaSmart_iOS/Pods/"$2"/"$2
+  moduleName=$2
 
-    targer_dir=$1"/"$2
+  while read line
+  do
+  # 获取pod
+  if [[ $line == pod* ]]
+  then
 
-    if [  -d $new_dir ];then
+   array=(${line//,/ })  
 
-       if [  -d $targer_dir ];then
-
-          echo "复制到：$targer_dir"
-          sudo cp -R   $new_dir  $targer_dir 
-
-       fi
-
+   if [[ ${#array[@]}==3 ]]; then
+    element=${array[1]} 
+    version=${array[2]} 
+    if [[ ${array[7]} ]]; then
+      version=${array[7]}
     fi
+
+    element=(${element//\'})
+    version=(${version//\'})
+
+    if [[ "$element" == "$moduleName" ]]; then
+      echo $version
+      return
+    fi
+
+    targer_dir=${filename}"/"${element}
+    # echo "$targer_dir"
+    
+    if [  -d $targer_dir ];then
+      sum=`expr ${sum} + 1`
+      checkout $targer_dir $version
+    fi
+
+   fi
+  fi
+  done < $doc'/TuyaSmart_iOS/Podfile'
+
 }
 
+# 拉取模块在pod里的分支，
+function checkoutAndNewBranch(){
+  
+  doc=$1
 
-function getdir(){
-    for element in `ls $1`
-    do  
-        push $1"/"$element
-    done
+  branchName=$2
+
+  newbranchName=$3
+
+  cd $doc
+
+  git checkout $branchName
+
+  git branch  $newbranchName
+
+  git checkout $newbranchName
+
+  git push origin $newbranchName
+
 }
 
 function push(){
@@ -40,7 +84,7 @@ function push(){
   result=`git commit -m "update"`
   if [[ $result != *nothing* ]]; then
     echo -e "\n$1/$element"
-    echo "${result}"
+    echo "update:${element}"
     git push origin
   else
     echo "-------------------no changed----------------------------"
@@ -48,12 +92,77 @@ function push(){
 
 }
 
+function copy(){
+
+    new_dir=$1"/TuyaSmart_iOS/Pods/"$2"/"$2
+
+    targer_dir=$1"/"$2
+
+    # #判断目录是否修改
+    cd  $1"/TuyaSmart_iOS"
+    result=`git status -s $new_dir`
+    if [[ -z "$result" ]]; then
+      return
+    fi
+
+    if [  -d $new_dir ];then
+       if [  -d $targer_dir ];then
+          
+          podModuleBranchName=$(getPodModuleBranchName $1 $2)
+          nowModuleBranchName=$(getBranchName $targer_dir)
+          newModuleBranchName=$(getBranchName $1"/TuyaSmart_iOS")
+
+          # 修改的模块创建新分支push
+          if [[ "$newModuleBranchName" != "$nowModuleBranchName" ]]; then
+            read  -p "是否新建模块$2分支${newModuleBranchName} (1:是 0:否) " -n 1 add
+            echo -e "\n"
+
+            if [[ $add == "1" ]]; then
+
+              #创建新分支
+              checkoutAndNewBranch $targer_dir $podModuleBranchName $newModuleBranchName 
+              echo "复制到：$targer_dir"
+              sudo cp -R   $new_dir  $targer_dir 
+
+              push $targer_dir
+            else
+              pushMaster="0"
+              return 
+            fi
+          else
+            echo "复制到：$targer_dir"
+            sudo cp -R   $new_dir  $targer_dir 
+
+            push $targer_dir
+
+
+          fi
+       fi
+
+    fi
+}
+
+
+function getModuledir(){
+    for element in `ls $1`
+    do  
+        copy $1 $element
+    done
+}
+
+#模块全上传后执行
+pushMaster="1"
 
 read  -p "请输入项目根目录： " doc
 
 echo -e "start:...\n"
+
 # 获取模块文件
 getModuledir $doc
 
-# push
-getdir $doc
+if [[ "$pushMaster" == "1" ]]; then
+  push $1"/TuyaSmart_iOS"
+fi
+
+echo -e "end...\n"
+
