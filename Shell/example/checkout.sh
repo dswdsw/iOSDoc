ac68a96af1c9c34b9fb132110991faaeac484522
stephen <dingsw@tuya.com>
2019-12-30 15:32:54 +0800

    add

--- Shell/example/checkout.sh
+++ Shell/example/checkout.sh
@@ -4,6 +4,9 @@ RED_COLOR='\033[31m'
 
 RESET='\033[0m'
 
+#配置密码
+pc_password="dswdsw"
+
 
 read  -p "请输入目录： " doc
 echo "目录为: $doc"
@@ -11,13 +14,13 @@ read  -p "请输入分支名： " branchName
 
 cd $doc
 
-git checkout . && git clean -xdf
+echo $pc_password | sudo -S git checkout . && git clean -xdf
 
-git checkout $branchName
+echo $pc_password | sudo -S git checkout $branchName
 
-git branch -u origin/$branchName
+echo $pc_password | sudo -S git branch -u origin/$branchName
 
-git fetch origin $branchName
+echo $pc_password | sudo -S git fetch origin $branchName
 
 echo -e  "${RED_COLOR}=== done ===${RESET}"
 
